00777ee2bbf035e4592ef5bf0dba80c8c3987b70
Enrico Cornacchia <enrico.cornacchia@studio.unibo.it>
2023-11-04 13:35:30 +0100

    created a support class to get the entire path from the graph

---
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/PathFinder.java
@@ -0,0 +1,37 @@
+package it.unibo.generics.graph.impl;
+
+import java.util.LinkedList;
+import java.util.List;
+
+class PathFinder<N> {
+    private final PathFinder<N> previous;
+    private final N current;
+
+    PathFinder(final N pos) {
+        this(null, pos);
+    }
+
+    PathFinder(final PathFinder<N> previous, final N pos) {
+        this.previous = previous;
+        this.current = pos;
+    }
+
+    N getPosition() {
+        return this.current;
+    }
+
+    public List<N> visualPath() {
+        final List<N> pathList = new LinkedList<>();
+        PathFinder<N> node = this;
+        do {
+            pathList.add(0, node.current);
+            node = node.previous;
+        } while(node != null);
+        return pathList;
+    }
+
+    public String toString() {
+        return "PathFinder [previous=" + previous + ", current=" + current + "]";
+    }
+
+}
